46327c1a08803a33dda6d5cb3c3d57e035af5ade
Caio Yoshida <caio.yoshida@yahoo.com.br>
2020-01-04 22:30:55 -0300

    chore: Reset password function

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const moment = require('moment')
 const crypto = require('crypto')
 const User = use('App/Models/User')
 const Mail = use('Mail')
@@ -36,6 +37,34 @@ class ForgotPasswordController {
       console.log('saiu')
     } catch (err) {
       return response.status(err.status).send({ error: { message: 'Invalid e-mail' } })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response
+          .status(401)
+          .send({ error: { message: 'Token expirado' } })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Falha ao atulizar senha' } })
     }
   }
 }
